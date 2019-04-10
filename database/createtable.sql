ba72925aa7579dbaa5a69662fbb27d33883823ef
lucnquyen <lucnquyen@gmail.com>
2019-04-10 22:52:18 +0700

    add database

---
+++ database/createtable.sql
@@ -0,0 +1,66 @@
+create database  mini;
+use mini;
+
+CREATE TABLE role(
+	id bigint NOT NULL PRIMARY KEY auto_increment,
+    name VARCHAR(255) NOT NULL,
+    code VARCHAR(255) NOT NULL,
+    createddate TIMESTAMP NULL,
+    modifieddate TIMESTAMP NULL,
+    createdby VARCHAR(255) NULL,
+    modifiedby VARCHAR(255) NULL
+);
+
+CREATE TABLE user(
+	id bigint NOT NULL PRIMARY KEY auto_increment,
+    name VARCHAR(255) NOT NULL,
+	passwork VARCHAR(255) NOT NULL,
+	fullname VARCHAR(255) NOT NULL,
+	status VARCHAR(255) NOT NULL,
+    roleid bigint NOT NULL,
+    createddate TIMESTAMP NULL,
+    modifieddate TIMESTAMP NULL,
+    createdby VARCHAR(255) NULL,
+    modifiedby VARCHAR(255) NULL
+);
+
+ALTER TABLE user ADD CONSTRAINT fk_user_role FOREIGN KEY (roleid) REFERENCES role(id); 
+
+CREATE TABLE news(
+	id bigint NOT NULL PRIMARY KEY auto_increment,
+	title VARCHAR(255) NULL,
+    thumbnail VARCHAR(255) NULL,
+	shortdescription TEXT NOT NULL,
+	content TEXT  NULL,
+    categoryid bigint NOT NULL,
+    createddate TIMESTAMP NULL,
+    modifieddate TIMESTAMP NULL,
+    createdby VARCHAR(255) NULL,
+    modifiedby VARCHAR(255) NULL
+);
+
+CREATE TABLE category(
+	id bigint NOT NULL PRIMARY KEY auto_increment,
+	name VARCHAR(255) NOT NULL,
+    code VARCHAR(255) NOT NULL,
+    createddate TIMESTAMP NULL,
+    modifieddate TIMESTAMP NULL,
+    createdby VARCHAR(255) NULL,
+    modifiedby VARCHAR(255) NULL
+);
+
+ALTER TABLE news ADD CONSTRAINT fk_news_category FOREIGN KEY (categoryid) REFERENCES category(id); 
+
+CREATE TABLE comment(
+	id bigint NOT NULL PRIMARY KEY auto_increment,
+	content TEXT NOT NULL,
+    user_id bigint NOT NULL,
+	new_id bigint NOT NULL,
+    createddate TIMESTAMP NULL,
+    modifieddate TIMESTAMP NULL,
+    createdby VARCHAR(255)  NULL,
+    modifiedby VARCHAR(255) NULL
+);
+
+ALTER TABLE comment ADD CONSTRAINT fk_comment_user FOREIGN KEY (user_id) REFERENCES user(id); 
+ALTER TABLE comment ADD CONSTRAINT fk_comment_news FOREIGN KEY (new_id) REFERENCES news(id); 
--- src/main/java/com/hcmute/model/UserModel.java
+++ src/main/java/com/hcmute/model/UserModel.java
@@ -1,10 +1,12 @@
 package com.hcmute.model;
 
-public class UserModel {
+public class UserModel extends AbstractModel<UserModel>{
 	
 	private String userName;
 	private String fullName;
-	
+	private String passwork;
+	private int status;
+	private Long rodeId;
 	public String getUserName() {
 		return userName;
 	}
@@ -17,7 +19,23 @@ public class UserModel {
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
-	
-	
+	public String getPasswork() {
+		return passwork;
+	}
+	public void setPasswork(String passwork) {
+		this.passwork = passwork;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public Long getRodeId() {
+		return rodeId;
+	}
+	public void setRodeId(Long rodeId) {
+		this.rodeId = rodeId;
+	}
 	
 }
